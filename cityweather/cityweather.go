20b1f6c0392dcfa1657295c2885affe8d6298c2f
Seungjin Kim <seungjin@seungjin.net>
2014-08-16 21:52:20 +0900

    cityweather module

---
+++ cityweather/cityweather.go
@@ -0,0 +1,60 @@
+package cityweather
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"appengine"
+	"appengine/urlfetch"
+)
+
+type Response struct {
+	Name    string    `json:"name"`
+	Weather []Weather `json:"weather"`
+	Main    Main      `json:"main"`
+}
+
+type Main struct {
+	Temp     float32
+	Pressure float32
+	Humidity float32
+	Temp_min float32
+	Temp_max float32
+}
+
+type Weather struct {
+	Main        string
+	Description string
+}
+
+func CityWeather(r *http.Request, cityname string) (string, float32, float32, float32) {
+
+	weather_info := openweather_request(r, cityname)
+
+	weather := &Response{}
+
+	err := json.Unmarshal(weather_info, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	return weather.Weather[0].Description, weather.Main.Temp, weather.Main.Temp_min, weather.Main.Temp_max
+}
+
+//http://api.openweathermap.org/data/2.5/weather?q=
+
+func openweather_request(r *http.Request, cityname string) []byte {
+	c := appengine.NewContext(r)
+	client := urlfetch.Client(c)
+	resp, err := client.Get("http://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=metric")
+	if err != nil {
+		os.Exit(1)
+	}
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		os.Exit(1)
+	}
+	return contents
+}
--- myapp/myapp.go
+++ myapp/myapp.go
@@ -4,12 +4,13 @@ import "fmt"
 import "net/http"
 
 import "mymodule"
+import "cityweather"
 
 func init() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/test1", testFunc1)
 	http.HandleFunc("/mymodule_dummyfunc1", call_module1_dummyfunc1)
-
+	http.HandleFunc("/cityweather", call_cityweather)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,4 +19,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func call_module1_dummyfunc1(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, mymodule.DummyFunc1())
+}
+
+func call_cityweather(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprint(w, "<p>City Weather</p>")
+
+	city_list := []string{"seoul", "newyork", "beijing", "london"}
+
+	for _, city := range city_list {
+		weather_description, temp, temp_min, temp_max := cityweather.CityWeather(r, city)
+		fmt.Fprintf(w, "<li> %s: %s (Temp: %.2f, [Min:%.2f, Max:%.2f])", city, weather_description, temp, temp_min, temp_max)
+	}
+
 }
